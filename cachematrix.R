e03732335688d056f586e0d8d2a87d5466723549
gsbirajdar <gsbirajdar516@gmail.com>
2015-07-23 17:17:27 +0530

    programming assignment 2 files

--- cachematrix.R
+++ cachematrix.R
@@ -4,8 +4,16 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
-        
+                inverse <- NULL ##command initializes the outpu matrix inverse to NULL
+                set <- function(y){
+                        x <<- y
+                        inverse <- NULL
+                }
+                
+                get <- function() x
+                setinv <- function(solve) inverse <<- solve
+                getinv <- function() inverse
+                list(set = set, get = get, setinv = setinv, getinv = getinv)
         
 }
 
